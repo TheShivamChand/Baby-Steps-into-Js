79cd1629368f3192c3ce486cbadd7d6bf4da5015
TheShivamChand <shivamdeopa@gmail.com>
2019-07-13 20:58:41 +0530

    Updated

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -2,7 +2,21 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-function generateTarget(){
+const generateTarget= () => {
 	return Math.floor(Math.random()*10);
 }
 
+const compareGuesses = (humanGuess,computerGuess,secretNumber) => {
+	const humanDiff=Math.abs(secretNumber-humanGuess);
+	const computerDiff=Math.abs(secretNumber-computerGuess);
+	return humanDiff<=computerDiff;
+}
+
+const updateScore = stringVal => {
+	if(stringVal==='human')
+		humanScore++;
+	else
+		computerScore++;
+}
+
+const advanceRound = () => currentRoundNumber++;
